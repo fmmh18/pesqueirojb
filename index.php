c10fbd9eecc9c584ba96cfe563b6a1751bacb457
HayashidaN <fmmh18@gmail.com>
2019-10-15 06:38:22 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,339 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>.::PESQUEIRO &amp; POUSADA JB::. </title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head>
+<!--
+Newline Template
+http://www.templatemo.com/tm-503-newline
+-->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+        <script src="https://use.fontawesome.com/c819d5b8ec.js"></script>
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <style type="text/css">
+ 
+.style1 {
+	color: #FFBB05;
+	font-size: smaller;
+}
+body {
+	background-color: #FFFFFF;
+	background-image: url();
+}
+.style2 {font-size: 36px}
+ 
+</style>
+    <body>
+
+        <div class="overlay"></div>
+        <section class="top-part">
+            <img src="img/placa-jb-1.jpg" style="background-size: cover;height:100%"/>
+        </section>
+        
+        <section class="cd-hero">
+
+          <div class="cd-slider-nav">
+            <nav>
+              <span class="cd-marker item-1"></span>
+              <ul>
+                <li class="selected"><a href="#0"><div class="image-icon"><img src="img/home-icon.png"></div>
+                <h6>INICIO</h6>
+                </a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/about-icon.png"></div><h6>A POUSADA</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/featured-icon.png"></div><h6>Pesca</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/projects-icon.png"></div><h6>Galeria</h6></a></li>
+                <li><a href="#0"><div class="image-icon"><img src="img/contact-icon.png"></div><h6>Reservas</h6></a></li>
+              </ul>
+            </nav> 
+          </div> <!-- .cd-slider-nav -->
+
+          <ul class="cd-hero-slider">
+
+            <li class="selected">
+              <div class="heading">
+                <h1>Seja Bem Vindo</h1>
+                <span>A pousada e Pesqueiro</span>
+              </div>
+              <br>
+              <br>
+              <div class="cd-full-width first-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content first-content">
+                        <h4 class="style1 style2">Pesqueiro & Rancho JB na tranquilidade do campo próximo da cidade! </h4>
+                        <p><span class="style2">Venha nos conhecer e descançar ou se preferir uma diversão para você e a sua família.</span><br>
+                        </p>
+                      </div>
+                    </div>
+
+                    <br>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>A Pousada</h1>
+                <span>Tudo para melhor lhe Atendê-lo</span> 
+              </div>
+              <div class="cd-half-width second-slide">   
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content second-content">
+                        <div class="row">
+                          <div class="col-md-7 left-image">
+                            <img src="img/left-about-image.jpg">
+                          </div>
+                          <div class="col-md-5">
+                            <div class="right-about-text">
+                              <h4>Nossa Estrutura</h4>
+                              <p>Disponibilizamos quartos para casal e solteiros.
+Uma ampla cozinha com churrasqueira, freezer e mesas para você preparar e desfrutar suas refeições.
+Para as crianças temos a piscina com cascata e uma quadra de areia contendo playground.
+Temos tanques para a modalidade pesque & pague com diversos peixes.</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-7">
+                            <div class="left-about-text">
+                              <h4>Diversão Garantida!</h4>
+                              <p>Se preferir pescar temos o deck de madeira na margem do rio e possuímos tambem barcos para podem ir em outros pontos do rio para uma boa pescaria.
+Se no final da pescaria estiver cansado porem não quiser ir para o quarto temos redário..</p>
+                            </div>
+                          </div>
+                          <div class="col-md-5 right-image">
+                            <img src="img/right-about-image.jpg">
+                          </div>
+                        </div>
+                      </div><br>
+
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>Pescaria, Lazer, Diversão!</h1>
+              </div>
+              <div class="cd-half-width third-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content third-content">
+                        <div class="row">
+                          <div class="col-md-7 left-image">
+                            <img src="img/left-feature-image.png">
+                          </div>
+                          <div class="col-md-5">
+                            <div class="right-feature-text">
+                              <h4>Pesca de Tablado, Barco e Tanque!</h4>
+                              <p>    Venha Pescar no Pesqueiro JB!
+Estrutura e ponto de apoio da pesca
+
+Sua pescaria na Pousada Sossego tem acompanhamento de estrutura de pesca e pontos de apoio. 
+
+Uma ótima opção para sua pescaria
+
+    O Pesqueiro JB fica as margens do rio Cuiabá no Distrito da guia, além disso, nossa pousada é um local excelente para quem quer pescar Jau, Caxara, Pintado ,Piauí,Pacu,Piraputanga,pacu peva e outros.
+
+Pesca
+
+Temos piloteiros próprios para plataformas e passeios de barcos. E contactamos com piloteiros terceirizados que fazem a pesca embarcada.
+
+Barcos e Plataformas
+
+Contamos com barcos hp 40, potencializando ainda mais sua pesca e também com plataformas estrategicamente posicionadas para sua pesca ser mais produtiva.</p>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>Um Pouco mais...</h1>
+                <span>Venha nos visitar</span> 
+              </div>
+              <div class="cd-half-width fourth-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content fourth-content">
+                        <div class="row">
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-01.jpg" data-lightbox="image-1"><img src="img/project-item-01.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-02.jpg" data-lightbox="image-1"><img src="img/project-item-02.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-03.jpg" data-lightbox="image-1"><img src="img/project-item-03.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-04.jpg" data-lightbox="image-1"><img src="img/project-item-04.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-05.jpg" data-lightbox="image-1"><img src="img/project-item-05.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-06.jpg" data-lightbox="image-1"><img src="img/project-item-06.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-07.jpg" data-lightbox="image-1"><img src="img/project-item-07.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-08.jpg" data-lightbox="image-1"><img src="img/project-item-08.jpg"></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>Faça sua Reserva Agora Mesmo!</h1>
+                <span>Contato e Informações pelos números e formulário abaixo!</span> 
+              </div>
+              <div class="cd-half-width fivth-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content fivth-content">
+                        <div class="row">
+                          <div class="col-md-4">
+                            <div class="left-info">
+                              <p>FAÇA SUA RESERVA!
+                              <br><br>
+                              <em>JOÃO BOSCO: +55 65 9 9952-9881<br>
+JUCA: +55 65 9 9949-6685<br>
+Email: jbscorreadacosta92@gmail.com</em>
+                              </p>
+                              <ul class="social-icons">
+                                <i><a href="https://fb.me/pesqueiroeranchoJB/"><i class="fa fa-facebook"></i></a></i>
+                                <i><a href="https://wa.me/5565999529881"><i class="fa fa-whatsapp"></i></a></i>
+                                 <i><a href="https://youtu.be/channel/UC-oLomF3HDK3zW9PGLMUAWw"><i class="fa fa-youtube"></i></a></i> 
+                              </ul>
+                            </div>
+                          </div>
+                          <div class="col-md-8">
+                            <div class="row">
+                              <form id="contact" action="contato" method="post">
+                                <div class="col-md-6">
+                                  <fieldset>
+                                    <input name="name" type="text" class="form-control" id="name" placeholder="Seu nome" required="">
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-6">
+                                  <fieldset>
+                                    <input name="email" type="email" class="form-control" id="email" placeholder="Seu Email" required="">
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <textarea name="message" rows="6" class="form-control" id="message" placeholder="Menssagem" required=""></textarea>
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <button type="submit" id="form-submit" class="btn">Enviar Menssagem</button>
+                                  </fieldset>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <br>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+          </ul> <!-- .cd-hero-slider -->
+        </section> <!-- .cd-hero -->
+
+
+        <footer>
+          <p>Todos Direitos &copy; <?=date("Y")?> Pesqueiro JB 
+                                
+        </footer>
+        <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content" style="background-color:#eaeaea">
+      <div class="modal-header" style="border-bottom:0px">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        
+      <h3 class="modal-title" id="TituloModalLongoExemplo">&nbsp;</h3>
+      </div>
+      <div class="modal-body"> 
+      <p class="float-center text-center"><img src="img/logo-pesqueiro-2.png" class="float-center text-center"/></p>
+      <p>&nbsp;</p>
+        <p>
+          De <b>1 de Outubro de <?=date("Y")?> a 31 de Janeiro de <?=(date("Y")+1)?>.</b> é o período de <b>piracema</b>, 
+          aonde se torna proibído a pescas nos  rios das Bacias Hidrográficas do Paraguai, Amazonas e Araguaia-Tocantins.
+        </p>
+        <p>Então se liga e evite transtornos e aproveite e já faça sua reserva para o período pós piracema conosco.</p>
+        <p>
+          <a class="btn btn-primary col-md-offset-1" style="border-radius:0px;border:0px;padding:10px !important" href="mailto:jbscorreadacosta92@gmail.com"><i class="fa fa-envelope fa-1x fa-inverse"></i> jbscorreadacosta92@gmail.com</a>
+          <a class="btn btn-success" style="border-radius:0px;border:0px;padding:10px !important" href="https://wa.me/5565999529881"><i class="fa fa-whatsapp fa-1x fa-inverse"></i> (65) 99952-9881</a>
+        </p>
+        <p>
+          <h4><b>Contato para agendamento:</b></h4>
+          Clique nos icones abaixo para conhecer nossas redes socias
+        </p>
+        <p>
+          <i><a href="https://fb.me/pesqueiroeranchoJB/"><i class="fa fa-facebook fa-2x fa-black"></i></a></i>  
+          <i><a href="https://youtu.be/channel/UC-oLomF3HDK3zW9PGLMUAWw"><i class="fa fa-youtube fa-2x fa-black"></i></a></i> 
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script type="text/javascript">
+
+$(document).ready(function() {
+    $('#modalExemplo').modal('show');
+})
+</script>
+    </body>
+</html>
